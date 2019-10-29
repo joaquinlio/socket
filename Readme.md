5c8c50031c30bc4af09ea4a9223a88e5254d3494
joaquinlio <joaquinlio97@gmail.com>
2019-10-29 15:45:33 -0300

    only exprresss

---
+++ Readme.md
@@ -0,0 +1,6 @@
+1 - Levantar el server en http en aws.
+2 - Acceder a http://127.0.0.1:3000/main
+3 - Chequear Log.
+4 - Integrar balanceador de AWS.
+5 - Chequear que el handler de certificados lo realice el balanceador.
+6 - Confirmar https.
--- server.js
+++ server.js
@@ -1,77 +1,19 @@
-const express = require("express");
-const next = require("next");
-require("isomorphic-fetch");
-const http = require("http");
+var app = require("express")();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http, {
+  pingTimeout: 3600000, // intervalo de una hora haga que la conexion del socket se cierre
+  pingInterval: 3600000
+});
 var fs = require("fs");
-/* var options = {
-  key: fs.readFileSync("./file.pem"),
-  cert: fs.readFileSync("./file.crt")
-}; */
-const socketIo = require("socket.io");
-const port = parseInt(process.env.PORT, 10) || 3001;
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
-const handle = app.getRequestHandler();
-//process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
-app.prepare().then(() => {
-  const server = express();
-  server.use(express.json());
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
 
-  const server2 = http.createServer(server);
-  const io = socketIo(server2, {
-    pingTimeout: 3600000, // intervalo de una hora haga que la conexion del socket se cierre
-    pingInterval: 3600000
-  });
+io.on("connection", function(socket) {
+  fs.appendFileSync("log.txt", "Conexión de usuario.\n");
+});
 
-  io.use(function(socket, next) {
-    console.log("socket conectado ID:" + socket.id);
-    next();
-    /* let token = socket.handshake.query.token;
-    fetch("https://api.axontraining.com/sesion", {
-      headers: {
-        Authorization: token
-      }
-    }).then(res => {
-      if (res.status == 200) {
-        console.log("socket conectado ID:" + socket.id);
-        next();
-      } else {
-        console.log("Socket: not authorized");
-      }
-    }); */
-  });
-  io.on("connection", socket => {
-    socket.on("clase", clase => {
-      socket.broadcast.emit("clase", {
-        clase
-      });
-    });
-  });
-  server.use(
-    "/static",
-    express.static(__dirname + "/static", {
-      maxAge: "365d"
-    })
-  );
-
-  /*  server.post("/socket/fin-clase", (req, res) => {
-    console.log(req.body);
-    io.on("connection", socket => {
-      socket.broadcast.emit("clase", JSON.parse(req.body));
-    });
-  }); */
-
-  server.get("*", (req, res) => {
-    //let sess = "xd";
-    return app.render(req, res, req.originalUrl, {
-      puerto: port
-    });
-  });
-  server.listen(port, err => {
-    if (err) throw err;
-    /* const host = server2.address().address;
-    const port2 = server2.address().port; */
-    console.log(`>Esto no es api PAPI Ready on https://localhost:${port}`);
-  });
+http.listen(3000, function() {
+  console.log("listening on *:3000");
 });
